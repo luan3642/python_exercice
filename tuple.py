071fd9479a158d210ef64a8383c695401b269631
luan3642 <luan3642@hotmail.com>
2024-01-10 22:41:54 -0300

    ...

--- tuple.py
+++ tuple.py
@@ -4,3 +4,28 @@ print(my_tuple[0])
 
 
 print(len(my_tuple))
+
+
+#tupla aninhada
+
+nested_tuple = (1, (1,2,3), ["luan", "jorge"])
+
+nested_tuple[2].append("lucas")
+
+print(len(nested_tuple))
+
+#tuple only item
+
+single_item_tuple = (50,)
+
+print(len(single_item_tuple))
+
+
+#desempacotar uma tupla
+
+tuplss = (20, 10, 30)
+
+x,y,z = tuplss
+
+
+print(x,y,z)
