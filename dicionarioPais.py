d165ab71a645ce3a580876ee40d03ea72b8c8f65
luan3642 <luan3642@hotmail.com>
2024-01-15 18:30:47 -0300

    ...

--- dicionarioPais.py
+++ dicionarioPais.py
@@ -1,10 +1,33 @@
-pais_capital = {
-    "Brasil": "Brasilia",
-    "Estados Unidos": "Washington",
-    "Alemanha" : "Berlim",
-    "Argentina" : "Buenos Aires",
-    "Australia" : "Camberra",
-    "Austria" : "Viena",
-    "Bahamas" : "Nassau"
+pais_capital = [
+    {
+    "Pais": "Brasilia",
+},
+    {
+        "Pais": "Washington",
+    },
+    {
+       "Pais" : "Berlim", 
+    },
 
-}
+    {
+       "Pais" : "Buenos Aires", 
+    },
+    
+    {
+        "Pais" : "Camberra",
+    },
+    
+    {
+       "Pais" : "Viena", 
+    },
+    
+    {
+        "Pais" : "Nassau"
+    }
+
+]
+
+ordena = sorted(pais_capital, key=lambda pais_capital : pais_capital["Pais"])
+
+for x in ordena:
+    print(x)
