7ef7bce3643e1a7617c4a5635e2a5f7ef01e2cb8
luan3642 <luan3642@hotmail.com>
2024-01-16 23:29:44 -0300

    ...

---
+++ booleano.py
@@ -0,0 +1,29 @@
+a = True
+b = False
+c = True
+
+result = a and b 
+
+result2 = c or b
+
+result3 = not c 
+
+print(result3)
+
+print(4 == 4)
+
+print(4!=3)
+
+print(5 < 2)
+
+print(5 > 3)
+
+
+print(bool(None))
+
+
+if a != True:
+    print("verdadeiro")
+else:
+    print("falso")
+
