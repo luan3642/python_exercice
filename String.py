426d4031e0e75f7b649eb3d0c647dbca88e162ab
luan3642 <luan3642@hotmail.com>
2024-01-17 22:34:17 -0300

    ...

---
+++ String.py
@@ -0,0 +1,18 @@
+nome = "luan"
+
+print(nome.upper())
+
+
+lastNome = "MAGALHAES"
+
+print(lastNome.lower())
+
+
+#removedor de espacos do inico ao fim
+
+string = " luan magalhaes "
+
+print(string.strip())
+
+
+print(string.replace("l", "x"))
