e3e882bc573bd9f569782190c1f23edc76e12127
luan3642 <luan3642@hotmail.com>
2024-01-18 23:18:35 -0300

    ...

--- String.py
+++ String.py
@@ -16,3 +16,43 @@ print(string.strip())
 
 
 print(string.replace("l", "x"))
+
+
+carros = "jaguar, polo, gol"
+
+
+nova_lista = carros.split(",")
+
+print(nova_lista)
+
+
+
+#concatenar string
+
+
+a = "hello"
+b = "world"
+
+
+junta = a +" "+ b
+
+print(junta)
+
+
+
+# nao pode combinar string e int junto
+
+# mas pode combinar usando o format
+
+idade = 10
+nomex = "ben {}"
+
+print(nomex.format(idade))
+
+
+# podemos usar caracter de escape para colocar aspas duplas numa string
+
+xxx = "luan \" senior \"cargo"
+
+
+print(xxx)
