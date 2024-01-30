fb3de8c217c4feda00fcabfbe138c9f15fd69e76
luan3642 <luan3642@hotmail.com>
2024-01-29 23:39:46 -0300

    ...

--- alternadoTupla.py
+++ alternadoTupla.py
@@ -10,3 +10,35 @@ z = tuple(tupla_nomes)
 
 for x in z:
     print(x)
+
+
+# adicionando um item a uma tupla
+    
+tupla_carros = ["Honda", "toyota"]
+lista = list(tupla_carros)
+lista.append("fiat")
+
+tupla_nova = tuple(lista)
+
+print(tupla_nova)
+
+
+# adicionando de outra forma
+
+tupla_new = ("luan",)
+
+x = ("caroline",)
+
+tupla_new += x
+print(tupla_new)
+
+
+#removendo um item da tupla
+
+dogs = ("pug", "golden retriver")
+dogs_list = list(dogs)
+dogs_list.remove("pug")
+
+dogs_tuple_changeable = tuple(dogs_list)
+
+print(dogs_tuple_changeable)
